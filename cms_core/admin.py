af8492b5c3a77f2781ece165917e33a84443d267
Endrju00 <andrzej.kapczynski21@gmail.com>
2024-01-06 18:01:37 +0100

    Change name of the class

--- cms_core/admin.py
+++ cms_core/admin.py
@@ -5,57 +5,57 @@ from django.http.request import HttpRequest
 from .models import Section, Skill, Image, Statistic, CV, History, Service, Project, Social, Message
 
 
-class UserQuerySet(admin.ModelAdmin):
+class RequestUserModelAdmin(admin.ModelAdmin):
     def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
         query = super().get_queryset(request).filter(user_id=request.user.id)
         return query
 
 @admin.register(Image)
-class ImageAdmin(UserQuerySet):
+class ImageAdmin(RequestUserModelAdmin):
     fields = ["image", "type", "user_id"]
     list_display = ["image_id", "image", "type", "user_id"]
 
 @admin.register(Section)
-class SectionAdmin(UserQuerySet):
+class SectionAdmin(RequestUserModelAdmin):
     fields = ["header", "description", "order", "visible", "type", "user_id", "image_id"]
     list_display = ["section_id", "header", "description", "order", "visible", "type", "user_id", "image_id"]
 
 @admin.register(Skill)
-class SkillAdmin(UserQuerySet):
+class SkillAdmin(RequestUserModelAdmin):
     fields = ["name", "value", "visibility", "section_id", "user_id"]
     list_display = ["skill_id", "name", "value", "visibility", "section_id", "user_id"]
 
 @admin.register(Statistic)
-class StatisticAdmin(UserQuerySet):
+class StatisticAdmin(RequestUserModelAdmin):
     fields = ["value", "label", "section_id", "user_id"]
     list_display = ["statistic_id", "value", "label", "section_id", "user_id"]
 
 @admin.register(CV)
-class CVAdmin(UserQuerySet):
+class CVAdmin(RequestUserModelAdmin):
     fields = ["file", "section_id", "user_id"]
     list_display = ["cv_id", "file", "section_id", "user_id"]
 
 @admin.register(History)
-class HistoryAdmin(UserQuerySet):
+class HistoryAdmin(RequestUserModelAdmin):
     fields = ["title", "subtitle", "description", "visible", "type", "date_started", "date_ended", "section_id", "user_id"]
     list_display = ["history_id", "title", "subtitle", "description", "visible", "type", "date_started", "date_ended", "section_id", "user_id"]
 
 @admin.register(Service)
-class ServiceAdmin(UserQuerySet):
+class ServiceAdmin(RequestUserModelAdmin):
     fields = ["title", "description", "order", "visible", "image_id", "section_id", "user_id"]
     list_display = ["service_id", "title", "description", "order", "visible", "image_id", "section_id", "user_id"]
 
 @admin.register(Project)
-class ProjectAdmin(UserQuerySet):
+class ProjectAdmin(RequestUserModelAdmin):
     fields = ["title", "description", "tag", "link", "visible", "image_id", "section_id", "user_id"]
     list_display = ["project_id", "title", "description", "tag", "link", "visible", "image_id", "section_id", "user_id"]
 
 @admin.register(Social)
-class SocialAdmin(UserQuerySet):
+class SocialAdmin(RequestUserModelAdmin):
     fields = ["title", "link", "image_id", "section_id", "user_id"]
     list_display = ["social_id", "title", "link", "image_id", "section_id", "user_id"]
 
 @admin.register(Message)
-class MessageAdmin(UserQuerySet):
+class MessageAdmin(RequestUserModelAdmin):
     fields = ["name", "email", "subject", "message", "user_id"]
     list_display = ["message_id", "name", "email", "subject", "message", "user_id"]
