3e7a36accd72eab73ff5b9fe91f78fae52769e88
Dominik Sawicki <dominik.sawicki52@gmail.com>
2024-01-14 18:17:29 +0100

    Add simple for loop logic

--- cms_core/views.py
+++ cms_core/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.template import loader
+from .models import Section
 
 # Create your views here.
 
 def website(request, user_id):
     # load user data etc.
+    sections = Section.objects.filter(user_id=user_id, visible=True).all()
     # data = 
-    # context = {
-    #     "latest_question_list": data,
-    # }
-    context = None
+    context = {
+        "sections": sections,
+    }
     
     return render(request, "cms_core/index.html", context)
